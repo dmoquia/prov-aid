c3bc801f87727ce2b1607508e56c4e2daf3fca67
dmoquia <dennis.moquia@gmail.com>
2023-06-12 17:33:27 +0800

    remove context wrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import JsonStateProvider from "./context/jsonState";
+// import JsonStateProvider from "./context/jsonState";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <JsonStateProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </JsonStateProvider>
+    {/* <JsonStateProvider> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    {/* </JsonStateProvider> */}
   </React.StrictMode>
 );
