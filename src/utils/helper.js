79a5aaa65f4aacc1013fb481a84ce8376c5cb2cb
dmoquia <dennis.moquia@gmail.com>
2023-07-14 15:41:40 +0800

    update code

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,59 +1,62 @@
 
-// export function processEmailString(emailString) {
-//     const lines = emailString.split('\n');
-//     let result = '';
-//     let deleteMobileNumber = false;
 
-//     for (let i = 0; i < lines.length; i++) {
-//       const line = lines[i].trim();
+// removes phone number from sender who is not from fusionconnect.com
+function replacePhoneNumber(blob) {
+  var regex = /[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}/g;
+  var matches = blob.match(regex);
+  var senderEmailMatch = blob.match(/From: (.*?)\n/);
+  var senderEmail = senderEmailMatch ? senderEmailMatch[1] : null;
 
-//       if (line.startsWith('From:')) {
-//         deleteMobileNumber = !line.includes('@fusionconnect.com');
-//       }
-
-//       if (deleteMobileNumber && (line.startsWith('Mobile:') || line.startsWith('Phone:'))) {
-//         result += line.substring(0, line.indexOf(':') + 1) + '\n';
-//       } else {
-//         result += line + '\n';
-//       }
-//     }
-
-//     return result;
-//   }
-export function processEmailString(emailString) {
-  const lines = emailString.split("\n");
-  let result = "";
-  let deleteMobileNumber = false;
-
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i].trim();
-
-    if (line.startsWith("From:")) {
-      deleteMobileNumber = !line.includes("@fusionconnect.com");
-    }
-
-    const lowercaseLine = line.toLowerCase(); // Convert line to lowercase
-
-    if (
-      (deleteMobileNumber &&
-        (lowercaseLine.startsWith("mobile:") ||
-          lowercaseLine.startsWith("mobile") ||
-          lowercaseLine.startsWith("phone:") ||
-          lowercaseLine.startsWith("phone") ||
-          lowercaseLine.startsWith("tel:") ||
-          lowercaseLine.startsWith("tel"))) ||
-      lowercaseLine.startsWith("telephone:") ||
-      lowercaseLine.startsWith("telephone") ||
-      lowercaseLine.startsWith("office:") ||
-      lowercaseLine.startsWith("office") ||
-      lowercaseLine.startsWith("o:") ||
-      lowercaseLine.startsWith("o")
-    ) {
-      result += line.substring(0, line.indexOf(":") + 1) + "\n";
-    } else {
-      result += line + "\n";
-    }
+  if (senderEmail && !senderEmail.includes("@fusionconnect.com")) {
+    blob = blob.replace(matches[0], "");
   }
 
-  return result;
+  return blob;
 }
+
+
+// convert blob of string into an array
+function splitEmailText(emailText) {
+  const delimiter = "From:";
+  const lines = emailText.split("\n");
+  const result = [];
+  let currentEmail = "";
+
+  for (let line of lines) {
+    if (line.startsWith(delimiter)) {
+      if (currentEmail !== "") {
+        result.push(currentEmail.trim());
+        currentEmail = "";
+      }
+    }
+    currentEmail += line + "\n";
+  }
+
+  if (currentEmail !== "") {
+    result.push(currentEmail.trim());
+  }
+
+  // Add a newline character after each "From:" line
+  const formattedResult = result.map((email) =>replacePhoneNumber(email.trim())  + "\n\n\n" ).join("");
+
+  return formattedResult;
+}
+
+
+// this function removes domain from an email
+export const modifyEmails = (emailString) => {
+  const emailRegex = /([\w.-]+)@([\w.-]+)/g;
+
+  const modifiedString = emailString.replace(emailRegex, (match, p1, p2) => {
+    if (p2 === 'fusionconnect.com') {
+      return match;
+    }
+    return p1;
+  });
+
+  const processedString = splitEmailText(modifiedString);
+
+  return processedString;
+
+};
+
